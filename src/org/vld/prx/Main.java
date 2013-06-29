1c3af1c6c185f74b7106ad4cc8eed70314324523
Ana <ana@dell.com>
2013-06-29 23:26:51 +0300

    first

---
+++ src/org/vld/prx/Main.java
@@ -0,0 +1,26 @@
+package org.vld.prx;
+
+import java.util.*;
+
+public class Main
+{
+	
+	Collection<?> col = new LinkedList<String>();
+	
+	public void call(String m)
+	{
+		System.out.println("string");
+	}
+	
+	public void call(Object o)
+	{
+		
+		System.out.println("object");
+	}
+	
+	public static void main(String ... s)
+	{
+		System.out.println(12345 + 5432l);
+		new Main().call("mama");
+	}
+}
